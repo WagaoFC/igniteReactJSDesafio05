4bf7004b9c98ae39a279e9de3791127b4cbb123c
WagaoFC <wagnerferreira_07@hotmail.com>
2022-12-29 00:30:18 -0300

    alterando displayName dos componentes, adicionando argTypes e a11y

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -2,18 +2,25 @@ import type { StoryObj, Meta } from '@storybook/react'
 import { Avatar, AvatarProps } from '@ignite-ui/react'
 
 export default {
-    title: 'Data display/Avatar',
-    component: Avatar,
-    args: {
-        src: 'https://github.com/wagaofc.png',
-        alt: 'Wagner Ferreira Costa',
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/wagaofc.png',
+    alt: 'Wagner Ferreira Costa',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
     },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
 
 export const WithFallback: StoryObj<AvatarProps> = {
-    args: {
-        src: undefined,
-    },
+  args: {
+    src: undefined,
+  },
 }
